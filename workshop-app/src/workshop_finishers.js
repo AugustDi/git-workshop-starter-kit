020a575d2552a728e3c84d53907aa1dfff1c11c6
Oleksandr Loktiev <sashaloktev2014@gmail.com>
2024-12-05 10:36:18 +0200

    Added myself to a file

--- workshop-app/src/workshop_finishers.js
+++ workshop-app/src/workshop_finishers.js
@@ -1,8 +1,13 @@
 const persons = [
-    {
-        "id": 1,
-        "name": "Example Name",
-        "time": "2:45:00"
-    },
+	{
+		"id": 1,
+		"name": "Example Name",
+		"time": "2:45:00"
+	},
+	{
+		"id": 2,
+		"name": "Oleksandr Loktiev",
+		"time": "10:19:00"
+	},
 
 ]
